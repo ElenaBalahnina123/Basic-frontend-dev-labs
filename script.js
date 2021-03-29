eb58dccc083894285a7ee0588f33000aef69c2b4
ElenaBalahnina123 <78481040+ElenaBalahnina123@users.noreply.github.com>
2021-03-29 19:29:42 +0300

    commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function showAlert() {
+   if(document.someVariable == null) {
+      document.someVariable = prompt("Введите имя:");
+   }
+   alert("Я это сделал, а " + document.someVariable + " тебе от меня совет: “ Поступай правильно — поступай в ВятГУ!”");
+}
+
