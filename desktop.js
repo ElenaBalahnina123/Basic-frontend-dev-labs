00ce068e43d7cfc1dff786505cd32d113f2ef0d0
ElenaBalahnina123 <78481040+ElenaBalahnina123@users.noreply.github.com>
2021-04-01 00:14:42 +0300

    Update desktop.js

--- desktop.js
+++ desktop.js
@@ -1,18 +1,18 @@
-// window.onload = function () {
-//     document.getElementById("content").style.visibility = "hidden"
-//     setTimeout(function () {
-//         document.getElementById("progress").style.visibility = "hidden"
-//         document.getElementById("content").style.visibility = "visible"
-//     }, 543)
+window.onload = function () {
+    document.getElementById("content").style.visibility = "hidden"
+     setTimeout(function () {
+       document.getElementById("progress").style.visibility = "hidden"
+        document.getElementById("content").style.visibility = "visible"
+     }, 543)
 
-//     var buttons = document.getElementsByTagName("button")
+     var buttons = document.getElementsByTagName("button")
 
-//     let onClickFunction = function() {
-//         alert("Макет был реализован Балахниной Еленой Павловной. Вариант № 1")
-//     }
+     let onClickFunction = function() {
+        alert("Макет был реализован Балахниной Еленой Павловной. Вариант № 1")
+     }
 
-//     for(let i = 0; i < buttons.length; i++) {
-//         let button = buttons[i]
-//         button.onclick = onClickFunction
-//     }
-// }
+    for(let i = 0; i < buttons.length; i++) {
+         let button = buttons[i]
+         button.onclick = onClickFunction
+     }
+ }
