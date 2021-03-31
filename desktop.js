e3c24cfc9e1d21822d2db4da7054828a8e9d8b15
ElenaBalahnina123 <78481040+ElenaBalahnina123@users.noreply.github.com>
2021-03-31 22:39:22 +0300

    commit3

---
+++ desktop.js
@@ -0,0 +1,18 @@
+window.onload = function () {
+    document.getElementById("content").style.visibility = "hidden"
+    setTimeout(function () {
+        document.getElementById("progress").style.visibility = "hidden"
+        document.getElementById("content").style.visibility = "visible"
+    }, 543)
+
+    var buttons = document.getElementsByTagName("button")
+
+    let onClickFunction = function() {
+        alert("Макет был реализован Балахниной Еленой Павловной. Вариант № 1")
+    }
+
+    for(let i = 0; i < buttons.length; i++) {
+        let button = buttons[i]
+        button.onclick = onClickFunction
+    }
+}
